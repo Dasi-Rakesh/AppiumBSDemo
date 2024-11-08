de55f9e334901f9d3d39276dc9af21045eabd11c
Rakesh Dasi <rakesh.d@browserstack.com>
2024-11-08 12:24:30 +0530

    Added Plugin for BrowserStack SDK

--- src/test/kotlin/DemoTest.kt
+++ src/test/kotlin/DemoTest.kt
@@ -1,27 +1,31 @@
 import io.appium.java_client.android.AndroidDriver
 import org.junit.jupiter.api.Test
+import org.openqa.selenium.JavascriptExecutor
 import org.openqa.selenium.remote.DesiredCapabilities
 import java.lang.Thread.sleep
 import java.net.URL
 
 class DemoTest {
     private val remoteAddress: String = "https://hub-cloud.browserstack.com/wd/hub"
-    private val bsUserName: String = System.getenv("USERNAME") ?: "username_undefined"
-    private val bsAccessKey: String = System.getenv("ACCESS_KEY") ?: "access_key_undefined"
-    private val appPath: String = System.getenv("APP_PATH") ?: "app_path_undefined"
-    private val deviceName: String  = "Google Pixel 7 Pro"
-    private val platform: String  = "android"
-    private val platformVersion: String  = "13.0"
 
     @Test
     fun sampleTest() {
-        lateinit var driver: AndroidDriver
+        var driver: AndroidDriver? = null
         try {
             driver = createDriver()
             println("Launched App")
             sleep(5000)
+
+            // Perform test steps here...
+
+            // Set session status as passed if test steps are successful
+            updateSessionStatus(driver, "passed", "Test completed successfully")
+        } catch (e: Exception) {
+            // Set session status as failed if an exception occurs
+            updateSessionStatus(driver, "failed", e.message ?: "An error occurred")
+            throw e
         } finally {
-            driver.quit()
+            driver?.quit()
         }
     }
 
@@ -32,19 +36,31 @@ class DemoTest {
 
     private fun getCapabilities(): DesiredCapabilities {
         val capabilities = DesiredCapabilities()
+
         // Appium capabilities
-        capabilities.setCapability("appium:deviceName", deviceName)
-        capabilities.setCapability("appium:app", appPath)
-        capabilities.setCapability("platformName", platform)
-        capabilities.setCapability("appium:platformVersion", platformVersion)
-        // BrowserStack capabilities
-        val options = HashMap<String, Any>()
-        options.put("userName", bsUserName)
-        options.put("accessKey", bsAccessKey)
-        options.put("projectName", "DemoProject")
-        options.put("buildName", "Test Build")
-        options.put("sessionName", "SampleTest")
-        capabilities.setCapability("bstack:options", options)
+        capabilities.setCapability("platformName", "android")
+        capabilities.setCapability("appium:platformVersion", "13.0")
+        capabilities.setCapability("appium:deviceName", "Google Pixel 7 Pro")
+        capabilities.setCapability("appium:app", "bs://5ec13c79a30aa813873e6dd2af8d6742a069cf4e")
+
+
+        // BrowserStack options
+        val bstackOptions = HashMap<String, Any>()
+        bstackOptions["userName"] = "USERNAME"
+        bstackOptions["accessKey"] = "ACCESS-KEY"
+        bstackOptions["projectName"] = "KHealthDemoProjectRun"
+        bstackOptions["buildName"] = "KHealthDemoRun"
+        bstackOptions["sessionName"] = "SampleTest"
+        capabilities.setCapability("bstack:options", bstackOptions)
+
         return capabilities
+    }
+
+    private fun updateSessionStatus(driver: AndroidDriver?, status: String, reason: String) {
+        if (driver is JavascriptExecutor) {
+            (driver as JavascriptExecutor).executeScript(
+                "browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"$status\", \"reason\": \"$reason\"}}"
+            )
+        }
     }
 }
